04ffc9906e7f6234a2ea7a271b87374466a9fe67
Larry Frank Rodríguez Pariona <101280306+LarryFrank0114@users.noreply.github.com>
2026-06-15 17:01:16 -0500

    Enhance login functionality with additional users

--- auth.py
+++ auth.py
@@ -22,12 +22,32 @@ def login_form():
                 if usuario.lower() == "larry" and contraseña == "admin123":
                     st.session_state.logged_in = True
                     st.session_state.user_role = "Administrador"
-                    st.session_state.username = "Larry Frank Rodriguez (Jefe de Almacenes)"
+                    st.session_state.username = "Larry Rodriguez - Jefe de Almacenes Externos"
                     st.rerun()
-                elif usuario.lower() == "supervisor" and contraseña == "super123":
+                elif usuario.lower() == "supervision" and contraseña == "super123":
                     st.session_state.logged_in = True
                     st.session_state.user_role = "Supervisor"
                     st.session_state.username = "Supervisor de Guardia SEDAPAL"
+                    st.rerun()
+                elif usuario.lower() == "piero" and contraseña == "piero123":
+                    st.session_state.logged_in = True
+                    st.session_state.user_role = "Administrador"
+                    st.session_state.username = "Piero Pezo - Encargado Almacén 10"
+                    st.rerun()
+                elif usuario.lower() == "marcial" and contraseña == "marcial123":
+                    st.session_state.logged_in = True
+                    st.session_state.user_role = "Administrador"
+                    st.session_state.username = "Marcial Huayta - Encargado Almacén 08"
+                    st.rerun()
+                elif usuario.lower() == "gregorio" and contraseña == "gregoriol123":
+                    st.session_state.logged_in = True
+                    st.session_state.user_role = "Administrador"
+                    st.session_state.username = "Gregorio Rodriguez - Almacén 01"
+                    st.rerun()
+                elif usuario.lower() == "kike" and contraseña == "kike123":
+                    st.session_state.logged_in = True
+                    st.session_state.user_role = "Administrador"
+                    st.session_state.username = "Enrique Sanchez - Encargado Almacén 06"
                     st.rerun()
                 else:
                     st.error("❌ Usuario o Contraseña no válidos.")
